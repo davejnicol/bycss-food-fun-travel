aa4e87492a6a3f21dd862312173cb1ff9849633d
Dave J Nicol <46354316+davejnicol@users.noreply.github.com>
2025-10-05 17:16:31 -0400

    Handling TinaCMS Content Modelling

--- tina/config.ts
+++ tina/config.ts
@@ -41,6 +41,53 @@ export default defineConfig({
             required: true,
           },
           {
+            type: "string",
+            name: "subtitle",
+            label: "Description",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "author",
+            label: "Author Name",
+            required: true,
+          },
+          {
+            type: "image",
+            name: "imageSrc",
+            label: "Featured Image",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "imageAlt",
+            label: "Alt Text for Featured Image",
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Published Date",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "tags",
+            label: "Tags",
+            list: true,
+            ui: {
+                component: "tags",
+                min: 1,
+            },
+            required: true,
+          },
+          {
+            type: "boolean",
+            name: "featured",
+            label: "Featured Post",
+            required: true,
+          },
+          {
             type: "rich-text",
             name: "body",
             label: "Body",
